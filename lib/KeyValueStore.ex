80723682a4c9a56a138fef3a214f79f68332eb9b
Naveen Negi <nav301186@gmail.com>
2016-12-21 07:15:29 +0530

    adding interface functions to KVStore

--- lib/KeyValueStore.ex
+++ lib/KeyValueStore.ex
@@ -13,4 +13,17 @@ def handle_call({:get, key}, state) do
   {HashDict.get(state,key), state}
 end
 
+#Create an abstraction so that User don't have to deal with
+
+def start do
+  GenericServerProcess.start(KVStore)
+end
+
+def put(pid, key, value) do
+  GenericServerProcess.call(pid, {:put, key, value })
+end
+
+def get(pid, key) do
+  GenericServerProcess.call(pid, {:get, key})
+end
 end
--- test/generic_server_process_test.exs
+++ test/generic_server_process_test.exs
@@ -2,10 +2,19 @@ defmodule GenericServerProcessTest do
   use ExUnit.Case
   doctest GenericServerProcess
 
-  test "the truth" do
+  test "should return the stored key" do
     pid = GenericServerProcess.start(KVStore)
     GenericServerProcess.call(pid,{:put,:some_key, :some_value})
     value =  GenericServerProcess.call(pid,{:get,:some_key})
     assert value == :some_value
   end
+
+
+    test "the truth" do
+      pid = KVStore.start
+      KVStore.put(pid, :some_key, :some_value)
+      value = KVStore.get(pid, :some_key)
+      assert value == :some_value
+    end
+
 end
